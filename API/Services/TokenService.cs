ad5bc951e2c0f8167189012778a3a0cd404c04c7
Pasha3amok <3amok1985@gmail.com>
2025-05-04 21:13:28 +0300

    added jwt

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -3,10 +3,9 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 using API.Entities;
-using API.interfaces;
 using Microsoft.IdentityModel.Tokens;
 
-namespace API.Services;
+namespace API.interfaces;
 
 public class TokenService(IConfiguration config) : ITokenService
 {
