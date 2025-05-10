6aadf3c7bd8202d613386fb6491e65035cb3691c
Pasha3amok <3amok1985@gmail.com>
2025-05-10 16:33:13 +0300

    .

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,26 +1,33 @@
-import { Component, inject, input} from '@angular/core';
+import {
+    Component,
+    inject,
+    input,
+    output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RegisterUser } from '../_models/RegisterUser';
 import { AccountService } from '../_services/account.service';
 import { UsersList } from '../_models/UsersList';
 
 @Component({
-  selector: 'app-register',
-  standalone: true,
-  imports: [FormsModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+    selector: 'app-register',
+    standalone: true,
+    imports: [FormsModule],
+    templateUrl: './register.component.html',
+    styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
     accService = inject(AccountService);
-    usersFromHomeComponent = input.required<UsersList[]>() ;
+    usersFromHomeComponent =
+        input.required<UsersList[]>();
+    cancelRegister = output<UsersList[]>();
     regModel: RegisterUser = new RegisterUser();
 
-    register(){
-        this.accService.register(this.regModel).subscribe();
+    register() {
+        this.accService
+            .register(this.regModel)
+            .subscribe();
     }
 
-    cancel(){
-
-    }
+    cancel() {}
 }
