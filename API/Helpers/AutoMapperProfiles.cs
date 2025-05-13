1506a5be0b98c108a1be06d0ec16217c0e391291
Pasha3amok <3amok1985@gmail.com>
2025-05-13 14:05:24 +0300

    added seed, userdata and mapper

---
+++ API/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,25 @@
+using System;
+using API.DTOs;
+using API.Entities;
+using API.Extensions;
+using AutoMapper;
+
+namespace API.Helpers;
+
+public class AutoMapperProfiles: Profile
+{
+	public AutoMapperProfiles()
+	{
+		CreateMap<AppUser, MemberDto>()
+		.ForMember(destination => destination.Age,
+			options => options.MapFrom(
+				sourse => sourse.DateOfBirth.CalculateAge()))
+			.ForMember(destination => destination.PhotoUrl,
+						options => options.MapFrom(
+							sourse => sourse.Photos.FirstOrDefault(
+								x => x.IsMain
+							)!.Url
+						));
+		CreateMap<Photo, PhotoDto>();
+	}
+}
